4ad0af42d06e1e3388a44ef21182b33b5c4d40e8
Vishwanath.Divatar <01fe24bca198@kletech.ac.in>
2025-11-19 15:36:54 +0530

    Another Multiply

---
+++ multi.py
@@ -0,0 +1,14 @@
+#Simple Python program for multiplication and division of two number
+# #Take input from user
+num1 = float(input("Enter first number: "))
+num2= float(input("Enter second number: "))
+ #Multiplication
+multiplication = num1 * num2
+#Division (with check to avoid division by zero)
+if num2 != 0:
+    division = num1 / num2
+else:
+    division = "undefined (cannot divide by zero)"
+# Display results
+print(f"\nMultiplication of ([num1] and [num2]) = {multiplication}")
+print(f"Division of {num1}) by (num2) = {division})")
